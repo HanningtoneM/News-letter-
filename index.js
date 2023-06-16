2d13cbff604a0046824291417d409d3b272bdcfd
HanningtoneM <hanningtonepompyy@gmail.com>
2023-06-16 18:49:42 +0300

    Fix JS issue

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const form = document.querySelector("form");
-const card_1 = document.querySelector(".card");
-const card_2 = document.querySelector(".card-2");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  card_1.classList.add("hide");
-  card_2.classList.remove("hide");
-});
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(form);
+    const value = data.get("email");
+  
+    if (
+      !value ||
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value) === false
+    ) {
+      const errorSpanElement = document.getElementById("error_span");
+      const inputElement = document.getElementById("email");
+  
+      errorSpanElement.classList.remove("hidden");
+  
+      inputElement.classList.add("error_email");
+      inputElement.value = "";
+  
+      setTimeout(() => {
+        inputElement.classList.remove("error_email");
+        errorSpanElement.classList.add("hidden");
+      }, 3000);
+    } else {
+      const successElement = document.querySelector(".success");
+      const signUpElement = document.querySelector(".sign-up");
+      const emailSpanElement = document.getElementById("email_span");
+  
+      signUpElement.style.animation = "fadeOut 1s";
+  
+      emailSpanElement.innerHTML = value;
+  
+      setTimeout(() => {
+        successElement.classList.remove("hidden");
+        signUpElement.classList.add("hidden");
+        successElement.style.animation = "fadeIn 1s";
+      }, 800);
+    }
+  }
